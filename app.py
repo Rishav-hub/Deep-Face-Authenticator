23c6ad2eb775d3c69ef25333f295d3e950b772a9
sethusaim <sethusaim@gmail.com>
2022-10-14 13:51:34 +0530

    removed debug=True and add application constants

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from starlette.responses import RedirectResponse
 
 from controller.app_controller import application
 from controller.auth_controller import authentication
+from face_auth.constant.application import APP_HOST, APP_PORT
 
 app = FastAPI()
 
@@ -22,4 +23,4 @@ app.add_middleware(SessionMiddleware, secret_key="!secret")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
